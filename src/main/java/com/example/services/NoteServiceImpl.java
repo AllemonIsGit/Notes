ab1cbbcb0652f4ad2c55eb6646ddac696bfeea99
the-metapod <novitzkimain@gmail.com>
2022-07-11 22:10:24 +0200

    update NoteServiceImpl class

--- src/main/java/com/example/services/NoteServiceImpl.java
+++ src/main/java/com/example/services/NoteServiceImpl.java
@@ -5,34 +5,33 @@ import com.example.domain.exception.NoteNotFoundException;
 import com.example.domain.model.Note;
 import com.example.repository.NoteRepository;
 import com.example.repository.NoteRepositoryImpl;
+import com.example.validation.ComplexValidators;
+import com.example.validation.abstractions.Validator;
 
 import java.util.List;
 
 public class NoteServiceImpl implements NoteService {
-    private static final String ILLEGAL_NOTE_EXCEPTION =
-            "Title has to be max 45 characters, and content has to be max 1000000 characters.";
 
     private final NoteRepository noteRepository;
+    private final Validator<Note> noteValidator;
 
     public NoteServiceImpl() {
         this.noteRepository = new NoteRepositoryImpl();
+        this.noteValidator = ComplexValidators.noteValidator();
     }
 
     @Override
     public void create(String title, String content) throws IllegalNoteException {
         Note note = new Note(title, content);
-        if (isValidNote(note)) {
-            noteRepository.save(note);
-        } else throw new IllegalNoteException(ILLEGAL_NOTE_EXCEPTION);
-
+        noteValidator.validateOrThrow(note, noteRepository::save, IllegalNoteException::new);
     }
 
     @Override
     public void update(int oldId, Note newNote) throws IllegalNoteException {
-        if (isValidNote(newNote)) {
-            noteRepository.update(oldId, newNote);
-        } else throw new IllegalNoteException(ILLEGAL_NOTE_EXCEPTION);
-
+        noteValidator.validateOrThrow(
+                newNote,
+                n -> noteRepository.update(oldId, n),
+                IllegalNoteException::new);
     }
 
     @Override
@@ -40,7 +39,7 @@ public class NoteServiceImpl implements NoteService {
         if (noteRepository.getById(id).isEmpty()) {
             System.out.println("There's no note with id " + id);
         } else
-        noteRepository.delete(id);
+            noteRepository.delete(id);
     }
 
     @Override
@@ -51,12 +50,7 @@ public class NoteServiceImpl implements NoteService {
 
     @Override
     public Note getById(int id) {
-        return noteRepository.getById(id)
-                .orElseThrow( () ->
-                        new NoteNotFoundException(String.format("Note with id %d was not found.", id)));
-    }
-
-    private boolean isValidNote(Note note) {
-        return note.getTitle().length() <= 43 && note.getContent().length() <= 1e6;
+        return noteRepository.getById(id).orElseThrow(() ->
+                new NoteNotFoundException(String.format("Note with id %d was not found.", id)));
     }
 }
